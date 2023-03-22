da829f110729e6489cb1f14d940ccc4f4cfd5537
muluo <mengchao1027@gmail.com>
2023-03-22 15:20:27 +0800

    skybox

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import * as BABYLON from '@babylonjs/core';
 import '@babylonjs/loaders';
 import '@babylonjs/inspector';
 
+BABYLON.SceneLoader.ShowLoadingScreen = false;
+
 const canvas = document.querySelector<HTMLCanvasElement>('#babylon-view')!;
 
 async function createEngine() {
@@ -24,6 +26,8 @@ class GameScene extends BABYLON.Scene {
   constructor(engine: BABYLON.WebGPUEngine) {
     super(engine);
     this.createCamera();
+    this.createSkyBox();
+
     this.createLight();
     this.loadModel();
   }
@@ -38,6 +42,12 @@ class GameScene extends BABYLON.Scene {
       this,
     );
 
+    camera.lowerBetaLimit = 0;
+    camera.upperBetaLimit = 2.5 * Math.PI;
+    camera.lowerRadiusLimit = 0;
+    camera.upperRadiusLimit = 500;
+    camera.panningDistanceLimit = 10;
+
     camera.attachControl();
   }
   // 创建灯光
@@ -51,6 +61,26 @@ class GameScene extends BABYLON.Scene {
     );
 
     directionalLight.intensity = 3;
+  }
+
+  createSkyBox() {
+    const skybox = BABYLON.MeshBuilder.CreateBox(
+      'skybox',
+      {
+        size: 1000.0,
+        sideOrientation: BABYLON.Mesh.BACKSIDE,
+      },
+      this,
+    );
+
+    const material = new BABYLON.BackgroundMaterial('skybox', this);
+    material.reflectionTexture = new BABYLON.CubeTexture(
+      '/texture/skybox',
+      this,
+      ['_px', '_py', '_pz', '_nx', '_ny', '_nz'].map((i) => `${i}.jpeg`),
+    );
+    material.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skybox.material = material;
   }
 
   // 创建立方体
@@ -70,6 +100,28 @@ class GameScene extends BABYLON.Scene {
       yoyo: true,
       duration: 1,
     });
+
+    const assetsArrayBuffer = await BABYLON.Tools.LoadFileAsync('/models/halo_4_forward_unto_dawn.glb', true);
+
+    const blob = new Blob([assetsArrayBuffer]);
+
+    const url = URL.createObjectURL(blob);
+
+    document.querySelector('button')?.addEventListener('click', async () => {
+      await BABYLON.SceneLoader.AppendAsync(url, undefined, this, undefined, '.glb');
+    });
+
+    // const coinList = this.getTransformNodeById('cruiser_1')!.getChildren() as BABYLON.Mesh[];
+
+    // coinList.forEach((coin) => {
+    //   coin.rotation = new BABYLON.Vector3(0, 0, 0);
+    //   gsap.to(coin.rotation, {
+    //     y: Math.PI * 2,
+    //     duration: 2,
+    //     repeat: -1,
+    //     ease: 'linear',
+    //   });
+    // });
   }
 }
 
